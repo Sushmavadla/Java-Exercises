2f47d2cdf17fb849923982403f1e9df3a26ddf21
Sushmavadla <sushmavadla27@gmail.com>
2024-06-26 16:52:17 +0530

    Add files via upload

---
+++ HashMapLab2.java
@@ -0,0 +1,12 @@
+package collections;
+import java.util.*;
+public class HashMapLab2 {
+public static void main(String args[]) {
+	Map<Integer, String> map= new HashMap<>();
+	map.put(60, "Virat");
+	map.put(100, "Dhoni");
+	map.put(120, "Hardik");
+	System.out.println(map.get(60));
+	
+}
+}
